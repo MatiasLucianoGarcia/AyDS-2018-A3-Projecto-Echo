9fe447847cc817b3748651ffe070687151f2327b
DESKTOP-6AIN7KL\matil <mati.luciano.garcia@gmail.com>
2018-04-11 09:58:12 -0300

    2.3 Se corrige la Interfaz StorageInterface

---
+++ ADictionaryEcho/app/src/main/java/model/StorageInterface.java
@@ -0,0 +1,12 @@
+package model;
+
+import android.content.Context;
+
+public interface StorageInterface {
+
+    void createNewDatabase(Context context);
+
+    void saveTerm(String term, String meaning);
+
+    String getMeaning(String term);
+}
