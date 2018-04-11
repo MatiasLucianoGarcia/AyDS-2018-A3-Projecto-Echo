fdcd16554dcb2ba7a17642213ebd296818f00d62
tfelder <tomas.felder@gmail.com>
2018-04-11 12:18:39 -0300

    2.9 Cambios en Model

--- ADictionaryEcho/app/src/main/java/model/DiccionarioModelConcreta.java
+++ ADictionaryEcho/app/src/main/java/model/DiccionarioModelConcreta.java
@@ -1,5 +1,15 @@
 package model;
 
+import android.text.Html;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
+
+import retrofit2.Response;
+
 public class DiccionarioModelConcreta implements DiccionarioModel {
 
     public DiccionarioModelConcreta() {}
@@ -9,6 +19,48 @@ public class DiccionarioModelConcreta implements DiccionarioModel {
     }
 
     public String retornarSignificado(String term) {
-         return DataBase.getMeaning(term);
+        String text = DataBase.getMeaning(term);
+
+        if (text != null) { // exists in db
+
+            text = "[*]" + text;
+        }
+        else {
+            Response<String> callResponse;
+            try {
+                callResponse = yandex.getTerm(term).execute();
+
+                Log.e("**","JSON " + callResponse.body());
+
+
+                if (callResponse.body() == null) {
+                    text = "No Results";
+                } else {
+
+
+                    Gson gson = new Gson();
+                    JsonObject jobj = gson.fromJson(callResponse.body(), JsonObject.class);
+
+                    // nouns
+                    String extract = jobj.get("text").getAsString();
+
+                    text = extract.replace("\\n", "<br>");
+                    text = textToHtml(text, term);
+
+                    // save to DB  <o/
+                    DataBase.saveTerm(textField1.getText().toString(), text);
+                }
+
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+
+        final String textToSet = text;
+        textPane1.post(new Runnable() {
+            public void run() {
+                textPane1.setText(Html.fromHtml(textToSet));
+            }
+        });
     }
 }
