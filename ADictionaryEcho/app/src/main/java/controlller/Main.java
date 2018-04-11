59fdb924c392e951f31d0b4c666eb652bb273c47
DESKTOP-6AIN7KL\matil <mati.luciano.garcia@gmail.com>
2018-04-11 12:20:58 -0300

    Comit de conflicto

--- ADictionaryEcho/app/src/main/java/controlller/Main.java
+++ ADictionaryEcho/app/src/main/java/controlller/Main.java
@@ -1,8 +1,10 @@
 package controlller;
 
+import view.VistaDiccionario;
+import view.VistaDiccionarioConcreta;
+
 /**
  * Created by tomas on 11/4/2018.
  */
 
-public class Main {
-}
+public class Main {}
