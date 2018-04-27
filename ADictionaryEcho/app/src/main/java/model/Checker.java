04a4cdd30b8bfe18c8c95df709655c7dcad22997
Unknown <mati.luciano.garcia@gmail.com>
2018-04-27 11:01:24 -0300

    7.12 Checker completo

--- ADictionaryEcho/app/src/main/java/model/Checker.java
+++ ADictionaryEcho/app/src/main/java/model/Checker.java
@@ -13,7 +13,20 @@ public class Checker {
     private Checker() {
     }
 
-    public boolean characterCheck(String s){
-        
+    public boolean isCorrect(String s){
+        return !(hasSpaceContinous(s)|| hasKeys(s) || hasBrackets(s));
+    }
+
+    private boolean hasSpaceContinous(String s){
+        return s.contains("  ");
+    }
+
+    private boolean hasKeys(String s){
+        return s.contains("{") || s.contains("}");
+    }
+
+    private boolean hasBrackets(String s){
+        return s.contains("[") || s.contains("]");
     }
 }
+
