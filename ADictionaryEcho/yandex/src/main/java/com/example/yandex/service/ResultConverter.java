6cda8f0ee1b60e1e6a9c195a8aec483684076623
tfelder <tomas.felder@gmail.com>
2018-05-30 10:45:11 -0300

    Se quita el modulo yandex del proyecto

--- ADictionaryEcho/yandex/src/main/java/com/example/yandex/service/ResultConverter.java
+++
@@ -1,5 +0,0 @@
-package com.example.yandex.service;
-
-public interface ResultConverter {
-    String createTranslatorResult(String resultString);
-}
--- ADictionaryEcho/yandex/src/main/java/com/example/yandex/service/TranslatorService.java
+++
@@ -1,5 +0,0 @@
-package com.example.yandex.service;
-
-public interface TranslatorService {
-    String callCreateTranslatedWord(String wordToTranslate) throws Exception;
-}
